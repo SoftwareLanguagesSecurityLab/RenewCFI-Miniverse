bdac0c866bc64f25ca8d72ad3f39bee967366db9
Erick Bauman <erick.bauman@utdallas.edu>
2018-09-28 23:25:22 -0500

    Continued progress on code generation, added code for patching jmps
    The code to patch jmp instructions will need to be moved into a
    second pass, because the mapping will not be complete until after
    the first pass.  However, I need to do the two passes differently
    than I did in the previous version, because for speed reasons we
    shouldn't do all the complex disassembly again.  Therefore I need
    to maintain a list of my own custom relocation entries to patch
    these targets after the first pass.  That way the second pass
    simply goes over each of these entries and patches them.

--- mapper.c
+++ mapper.c
@@ -2,9 +2,13 @@
 #include <string.h>
 
 #define NOP 0x90
+#define JMP_REL_SHORT 0xeb
+#define JMP_REL_NEAR 0xe9
 
 typedef struct mv_code_t{
   uint8_t *code;
+  uint32_t *mapping;
+  uint64_t base;
   size_t code_size;
   uint64_t offset;
 } mv_code_t;
@@ -19,8 +23,8 @@ uint8_t* gen_code(const uint8_t* bytes, size_t bytes_size,
   uint8_t result;
   mv_code_t code;
   code.offset = 0;
-  uint64_t base = address;
-  uint32_t* mapping = malloc( sizeof(uint32_t) * bytes_size );
+  code.base = address;
+  code.mapping = malloc( sizeof(uint32_t) * bytes_size );
   code.code = malloc( bytes_size ); // Will re-alloc to accommodate increased size
   code.code_size = bytes_size;
   
@@ -30,26 +34,8 @@ uint8_t* gen_code(const uint8_t* bytes, size_t bytes_size,
   while( result = ss_disasm_iter(handle, &bytes, &bytes_size, &address, insn) ){
     //printf("0x%llx: %s\t%s\n", insn->address, insn->mnemonic, insn->op_str);
     if( result == SS_SUCCESS ){
-      /* If we have selected a chunk size that is not zero, pad to chunk size
-         whenever we encounter an instruction that does not evenly fit within a
-         chunk.  Fill the padded area with NOP instructions.
-      */
-      if( chunk_size != 0 && (chunk_size - (code.offset % chunk_size) < insn->size) ){
-        memset(code.code+code.offset, NOP, chunk_size - (code.offset % chunk_size));
-	code.offset += chunk_size - (code.offset % chunk_size);
-      }
-      /* If we have a nonzero chunk size, then we need to ensure all call
-         instructions are padded to right before the end of a chunk.
-         TODO: Cover all variations of call instructions
-      */
-      if( chunk_size != 0 && (insn->id == X86_INS_CALL) && (code.offset % chunk_size < insn->size) ){
-        memset(code.code+code.offset, NOP, (code.offset % chunk_size) - insn->size);
-	code.offset += (code.offset % chunk_size) - insn->size;
-      }
-      mapping[insn->address-base] = code.offset; // Set offset of this instruction in mapping
+      code.mapping[insn->address-base] = code.offset; // Set offset of this instruction in mapping
       gen_insn(&code, chunk_size, insn);
-      /* TODO: change offset depending on whether instruction changed */
-      code.offset += insn->size;
     }else if( insn->id == X86_INS_JMP ){ // Special jmp instruction
       /* TODO: Patch special instruction */ 
       gen_insn(&code, chunk_size, insn);
@@ -59,12 +45,28 @@ uint8_t* gen_code(const uint8_t* bytes, size_t bytes_size,
     }
   }
 
-  free(mapping);
+  free(code.mapping);
   return code.code;
 }
 
 /* Generate a translated version of an instruction to place into generated code buffer. */
 void gen_insn(mv_code_t* code, size_t chunk_size, cs_insn *insn){
+  /* If we have selected a chunk size that is not zero, pad to chunk size
+     whenever we encounter an instruction that does not evenly fit within a
+     chunk.  Fill the padded area with NOP instructions.
+  */
+  if( chunk_size != 0 && (chunk_size - (code->offset % chunk_size) < insn->size) ){
+    memset(code->code+code->offset, NOP, chunk_size - (code->offset % chunk_size));
+    code->offset += chunk_size - (code->offset % chunk_size);
+  }
+  /* If we have a nonzero chunk size, then we need to ensure all call
+     instructions are padded to right before the end of a chunk.
+     TODO: Cover all variations of call instructions
+  */
+  if( chunk_size != 0 && (insn->id == X86_INS_CALL) && (code->offset % chunk_size < insn->size) ){
+    memset(code->code+code->offset, NOP, (code->offset % chunk_size) - insn->size);
+    code->offset += (code->offset % chunk_size) - insn->size;
+  }
   /* Expand allocated memory for code to fit additional instructions */
   /* TODO: place this in a location that accounts for different code size
      for generated instructions */
@@ -79,9 +81,29 @@ void gen_insn(mv_code_t* code, size_t chunk_size, cs_insn *insn){
   }else{
     memcpy(code->code+code->offset, insn->bytes, insn->size); // Copy insn's bytes to generated code 
   }
+  /* TODO: change offset depending on whether instruction changed */
+  code->offset += insn->size;
 }
 
 void inline gen_uncond(mv_code_t *code, cs_insn *insn){
+  uint32_t disp;
   memcpy(code->code+code->offset, insn->bytes, insn->size); // Copy insn's bytes into generated code 
   
+  switch( *(insn->bytes) ){
+    /* Jump with 4-byte offset */
+    case JMP_REL_NEAR:
+      /* Retrieve jmp target offset, look up rewritten destination in mapping, and patch address */
+      /* TODO: This approach will actually not work, because the mapping may not yet have an entry
+         for code that must be jumped *forward* to, so this code will need to be moved to a second
+         pass in which all statically identifiable targets are patched. */
+      disp = *(uint32_t)(insn->bytes+1);
+      disp = code->mapping[insn->address+disp-code->base] - code->offset;
+      memcpy(code->code+code->offset+1, disp, 4);
+      break;
+    /* Jump with 1-byte offset */
+    case JMP_REL_SHORT:
+      disp = *(uint8_t)(insn->bytes+1);
+      /* TODO: special case where we must extend the instruction to its longer form */
+      break;
+  }
 }
