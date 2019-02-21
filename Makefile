053e35abd7f63789eec894809435d3d91fe1dfa3
Erick Bauman <erick.bauman@utdallas.edu>
2019-02-20 21:28:04 -0600

    Started implementing new approach to loading library
    After some investigation, I have found that even with PIC flags,
    a binary is still laden with relocations that must be resolved at
    load time.  This makes simply dropping an arbitrary binary blob
    into an address space infeasible.  udis86 keeps tables of data for
    disassembly, and at least one table contains pointers to other
    tables.  These pointers must be updated to reflect wherever the
    binary ends up being loaded, and I think there will probably be
    relocation entries for any case in which data refers to other data.
    Therefore, even if we can eliminate absolute addresses in code, we
    may not be able to eliminate absolute addresses in data.
    
    However, I have found that the generated binary now only has a
    single type of relocation entry, R_386_RELATIVE, and I have decided
    to simply parse the relocation entries myself when I load the lib
    into memory.  Implementing this will allow me to either dynamically
    load the lib at runtime, or statically patch the lib and place it
    at a fixed address in a binary.  This still needs to be implemented
    and is the next step I intend to do.
    
    I also found that the data segment in the library was being loaded
    at a one-page offset from the text section when compared to its
    offset in the file.  This offset meant that any data references
    were wrong if I just tried to map the whole file in at once.  I
    have changed the loading process to parse the elf header and map
    in LOAD segments to their requested addresses.  I also zero out
    the bytes in the .bss section (manually, because .bss starts right
    after .data not at a page aligned address so I can't use mmap)
    because musl will try to read an address from .bss that would
    normally have been initialized at some point and uses that to try
    to perform system calls if its value is not zero.  When I map the
    data segment, the region that would contain .bss values actually
    contains bytes from sections like .comment and debug sections
    since .bss doesn't take up any space in the file, so I clear the
    bytes.  If anything relies on a value here or in .data that is
    set during initialization, we may have other issues in the future.

--- Makefile
+++ Makefile
@@ -2,15 +2,15 @@ MUSL_PATH = ~/git/musl/lib
 
 all: mapper.o
 	$(AR) -rsc libminiverse.a mapper.o
-	$(CC) -m32 -Wall -Wextra -g -fPIE -shared -static -pie -nostdlib dummy.c libminiverse.a /usr/local/lib/libssdis32.a /usr/local/lib/libudis86.a /usr/local/lib/libpagealloc.a handlers.c $(MUSL_PATH)/libc.a -lgcc -Wl,-wrap=mmap -Wl,-wrap=mprotect -Wl,-wrap=printf -o libminiversebin
+	$(CC) -m32 -Wall -Wextra -g -fPIE -shared -static -pie -nostdlib dummy.c libminiverse.a /usr/local/lib/libssdis.a /usr/local/lib/libudis86.a /usr/local/lib/libpagealloc.a handlers.c $(MUSL_PATH)/libc.a -lgcc -Wl,-wrap=mmap -Wl,-wrap=mprotect -Wl,-wrap=printf -o libminiversebin
 
 test: mapper.o
 	$(AR) -rsc libminiverse.a mapper.o
-	$(CC) -m32 -g driver.c libminiverse.a /usr/local/lib/libssdis32.a /usr/lib/libcapstone32.a -o driver
-	$(CC) -m32 -g driver2.c libminiverse.a -Ltests/ -ltest /usr/local/lib/libssdis32.a /usr/lib/libcapstone32.a -o driver2
-	$(CC) -m32 -g driver3.c handlers.c libminiverse.a -Ltests/ -ltest /usr/local/lib/libssdis32.a /usr/lib/libcapstone32.a -Wl,-wrap=mmap -Wl,-wrap=mprotect -o driver3
-	$(CC) -m32 -g driver4.c inittester.c -Ltests/ -ltest -o driver4
-	$(CC) -m32 -g driver5.c handlers.c libminiverse.a /usr/local/lib/libssdis32.a /usr/lib/libcapstone32.a -Wl,-wrap=mmap -Wl,-wrap=mprotect -o driver5
+	#$(CC) -m32 -g driver.c libminiverse.a /usr/local/lib/libssdis32.a /usr/lib/libcapstone32.a -o driver
+	#$(CC) -m32 -g driver2.c libminiverse.a -Ltests/ -ltest /usr/local/lib/libssdis32.a /usr/lib/libcapstone32.a -o driver2
+	#$(CC) -m32 -g driver3.c handlers.c libminiverse.a -Ltests/ -ltest /usr/local/lib/libssdis32.a /usr/lib/libcapstone32.a -Wl,-wrap=mmap -Wl,-wrap=mprotect -o driver3
+	#$(CC) -m32 -g driver4.c inittester.c -Ltests/ -ltest -o driver4
+	$(CC) -m32 -g driver5.c handlers.c libminiverse.a /usr/local/lib/libssdis.a /usr/local/lib/libudis86.a /usr/local/lib/libpagealloc.a $(MUSL_PATH)/libc.a -Wl,-wrap=mmap -Wl,-wrap=mprotect -o driver5
 	# No hooking version (to test without rewriting anything)
 	#$(CC) -m32 -g driver5.c libminiverse.a /usr/local/lib/libssdis32.a /usr/lib/libcapstone32.a -o driver5
 	$(CC) -m32 -g driver6.c inittester.c -o driver6
--- inittester.c
+++ inittester.c
@@ -27,8 +27,12 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <elf.h>
+#include <string.h>
+#include <errno.h>
 
-#define REGISTER_HANDLER_OFFSET 0x140803
+#define REGISTER_HANDLER_OFFSET 0x92fc
+//#define REGISTER_HANDLER_OFFSET 0x140803
 //#define REGISTER_HANDLER_OFFSET 0x1407bd
 //#define REGISTER_HANDLER_OFFSET 0x12b7a0
 //#define REGISTER_HANDLER_OFFSET 0x141cdd
@@ -37,15 +41,79 @@
 
 // An approximation of start of data section, rounded down to nearest page boundary
 //#define DATA_OFFSET_START 0x1d1000
-#define DATA_OFFSET_START 0x1e7000
+#define DATA_OFFSET_START 0x15000
+//#define DATA_OFFSET_START 0x1e7000
 //#define DATA_SIZE 0x114ff0
-#define DATA_SIZE 0x114fd0
+#define DATA_SIZE 0xd000
+//#define DATA_SIZE 0x114fd0
 
 // An approximation of start of bss section, rounded down to nearest page boundary
+// This contains fields that normally may be initialized behind the scenes,
+// but will not be initialized since we're just dropping this in.  Something
+// to investigate is __sysinfo, which appears to hold the address of pointers
+// in the vdso or something.  It appears that __vsyscall in musl reads from
+// __sysinfo-some_offset to get the code to perform a fast syscall, but if it
+// finds 0 instead it will use the safe slower fallback of using the int
+// instruction.  This is what I believe was happening when I was testing earlier
+// because syscalls were working fine previously, but after having the data/bss
+// sections get...misaligned(?) (another thing to investigate), the value at
+// the offset was filled with garbage, causing __vsyscall to call some random
+// address and crash.
 //#define BSS_OFFSET_START 0x2e5000
-#define BSS_OFFSET_START 0x2fb000
-#define BSS_SIZE 0x1b04
+#define BSS_OFFSET_START 0x21000
+//#define BSS_OFFSET_START 0x2fb000
+#define BSS_SIZE 0x2000
+//#define BSS_SIZE 0x1b04
 //#define BSS_SIZE 0x1af0
+
+/* Load binary at address specified.  Assume address is not NULL and that
+   we can guarantee that there is enough space at the specified address to
+   map the binary into memory without colliding with another mapped region */
+void load_binary(int fd, void* address){
+  Elf32_Ehdr* ehdr;
+  Elf32_Phdr* phdr;
+  Elf32_Shdr* shdr;
+  uintptr_t seg_addr,align_offset;
+  //void* ehdr_addr;
+  void* shdr_addr;
+  int i;
+  /* Map elf header into memory */
+  ehdr = (Elf32_Ehdr*)mmap(0, 0x1000, PROT_READ, MAP_PRIVATE, fd, 0);
+  /* Read phdr offset from ELF header */
+  printf("phdr offset: %d\n", ehdr->e_phoff);
+  phdr = (Elf32_Phdr*)((uintptr_t)ehdr+ehdr->e_phoff);
+  /* Iterate through phdr entries to find LOAD segments */
+  for( i = 0; i < ehdr->e_phnum; i++ ){
+    if( phdr->p_type == PT_LOAD ){
+      printf("Load offset: 0x%x addr: 0x%x\n", phdr->p_offset, phdr->p_vaddr );
+      /* Load segment at correct address relative to load address */
+      seg_addr = ((uintptr_t)address+phdr->p_vaddr) & 0xfffff000;
+      align_offset = ((uintptr_t)address+phdr->p_vaddr) & 0x00000fff;
+      mmap((void*)(seg_addr), phdr->p_memsz+align_offset,
+        (phdr->p_flags & PF_X) ? PROT_EXEC : PROT_READ|PROT_WRITE,
+        MAP_PRIVATE, fd, phdr->p_offset-align_offset );
+    }
+    phdr++;
+  }
+  /* Map section header table into memory (assume it doesn't exceed a page)
+     Unfortunately we can't map from a non-page-aligned offset, so we have to
+     map some of the file contents before the section header table. */
+  shdr_addr = (Elf32_Shdr*)mmap(0, 0x1000, PROT_READ, MAP_PRIVATE,
+    fd, ehdr->e_shoff & 0xfffff000);
+  shdr = (Elf32_Shdr*)((uintptr_t)shdr_addr+(ehdr->e_shoff & 0x00000fff));
+  printf( "error result: %d, %s\n", errno, strerror( errno ));
+  for( i = 0; i < ehdr->e_shnum; i++ ){
+    /* Assume that section of type SHT_NOBITS is .bss section, use address and
+       size to zero out bits in this region, overwriting garbage content
+       from sections following .bss */
+    if( shdr->sh_type == SHT_NOBITS ){
+      memset( address+shdr->sh_addr, 0, shdr->sh_size);
+    }
+    shdr++;
+  }
+  munmap(shdr_addr, 0x1000);
+  munmap(ehdr, 0x1000);
+}
 
 /* Map memory for miniverse library and set up handlers. 
    */
@@ -56,14 +124,15 @@ void* miniverse_init(){
     exit(0);
   }
   int fd = open("libminiversebin", O_RDONLY);
-  mmap((void*)0xa000000, st.st_size, PROT_EXEC, MAP_PRIVATE, fd, 0);
+  load_binary(fd, (void*)0xa000000);
+  //mmap((void*)0xa000000, st.st_size, PROT_EXEC, MAP_PRIVATE, fd, 0);
   close(fd);
   /* Set data section of library to be writable and not executable */
-  mprotect((void*)(0xa000000+DATA_OFFSET_START), DATA_SIZE, PROT_WRITE);
+  //mprotect((void*)(0xa000000+DATA_OFFSET_START), DATA_SIZE, PROT_WRITE);
   /* Map bss section with zeros */
   //mprotect((void*)(0xa000000+BSS_OFFSET_START), BSS_SIZE, PROT_WRITE);
-  munmap((void*)(0xa000000+BSS_OFFSET_START), BSS_SIZE);
-  mmap((void*)(0xa000000+BSS_OFFSET_START), BSS_SIZE, PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
+  //munmap((void*)(0xa000000+BSS_OFFSET_START), BSS_SIZE);
+  //mmap((void*)(0xa000000+BSS_OFFSET_START), BSS_SIZE, PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
   void (*register_handler)() = (void (*)())(0xa000000 + REGISTER_HANDLER_OFFSET);
   register_handler();
   return (void*)0x0; /* Do not actually return any pointer, as the handler will call gen_code */
